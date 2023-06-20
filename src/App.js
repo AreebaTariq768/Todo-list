5c453f62fa6893dc23e03fefab4ad20c2dc47fee
AreebaTariq768 <raoareeba2000@gmail.com>
2023-06-20 17:15:59 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './logo.svg';
+import {useState} from 'react'
+
 import './App.css';
 
 function App() {
+ const [newItem, setNewItem] = useState("");
+ const [todos, setTodos] = useState([]);
+ function handleSubmit(e) {
+  e.preventDefault()
+  setTodos(currentTodos => {
+    return [
+      ...currentTodos,
+      { id: crypto.randomUUID(),title :newItem , completed: false },
+    ]
+  })
+  setNewItem(" ")
+ }
+ function toggleTodo(id, completed) {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed}
+      }
+
+      return todo
+    })
+  })
+}
+function deleteTodo(id) {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id)
+  })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <form onSubmit={handleSubmit} className='new-item-form'>
+   <div className='form-row'>
+    <label htmlFor='item'> New Item</label>
+    <input type='text' value ={newItem} onChange={e => setNewItem(e.target.value) } id="item"></input>
+   </div>
+   <button className='btn'>Add</button>
+   </form>
+   <h1 className='header'>TODO List</h1>
+   <ul className='list'>
+  
+    {todos.map(todo =>{
+      return  <li key={todo.id}>
+      <label>
+        <input
+          type="checkbox" checked= {todo.completed} onChange={e =>toggleTodo(todo.id , e.target.checked)}
+        />
+        {todo.title}
+      </label>
+      <button  className="btn btn-danger"onClick={() =>deleteTodo(todo.id)} >
+        Delete
+      </button>
+    </li>
+    })}
+ 
+    </ul>
+   </>
   );
 }
 
